c31b2d4a8932c2929e5b556cd1e8177f7f31b228
Sidharth Malpani <135989098+areysid@users.noreply.github.com>
2025-09-21 22:26:26 +0530

    Update page.tsx

--- app/(invite)/(routes)/invite/[inviteCode]/page.tsx
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -9,67 +9,39 @@ interface InviteCodePageProps {
   };
 }
 
-const InviteCodePage = async ({
-  params
-}: InviteCodePageProps) => {
+const InviteCodePage = async ({ params }: InviteCodePageProps) => {
   const profile = await currentProfile();
 
-  if(!profile){
-    return <RedirectToSignIn />;
+  if (!profile) {
+    return <RedirectToSignIn />; // Only JSX returned when user not logged in
   }
 
-  if(!params.inviteCode) {
-    return redirect("/")
+  if (!params.inviteCode) {
+    redirect("/");
   }
 
   const existingServer = await db.server.findFirst({
-  where: {
-    inviteCode: params.inviteCode,
-    members: {
-      some: {
-        profileId: profile.id
-      }
-    }
-  }
-});
-
-if(existingServer){
-    return redirect(`/server/${existingServer.id}`);
-}
-
-// await db.server.update({
-//   where: { inviteCode: params.inviteCode },
-//   data: {
-//     members: {
-//       create: [{ profileId: profile.id }],
-//     },
-//   },
-// });
-
-// const server = await db.server.findUnique({
-//   where: { inviteCode: params.inviteCode },
-// });
-
-// if (!server) {
-//   return redirect("/"); // Or show a custom "invalid invite" page
-// }
-
-const server = await db.server.update({
-  where: { inviteCode: params.inviteCode },
-  data: {
-    members: {
-      create: [{ profileId: profile.id }],
+    where: {
+      inviteCode: params.inviteCode,
+      members: { some: { profileId: profile.id } },
     },
-  },
-});
+  });
 
-    if(server){
-        return redirect(`/server/${server.id}`)
-    }
+  if (existingServer) {
+    redirect(`/server/${existingServer.id}`);
+  }
 
-  return (
-    null
-  );
+  const server = await db.server.update({
+    where: { inviteCode: params.inviteCode },
+    data: { members: { create: [{ profileId: profile.id }] } },
+  });
+
+  if (server) {
+    redirect(`/server/${server.id}`);
+  }
+
+  return null; // Safe fallback
 };
+
 
 export default InviteCodePage;
